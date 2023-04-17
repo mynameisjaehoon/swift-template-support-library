6c0d380594f2d7e8db3c99139bcbb76377ebfabf
Jaehoon-So <jhspacelover@naver.com>
2023-04-17 20:29:51 +0900

    fix: fix package name

--- Package.swift
+++ Package.swift
@@ -6,23 +6,19 @@ import PackageDescription
 let package = Package(
     name: "SwiftTSL",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "swift-template-support-library",
-            targets: ["swift-template-support-library"]),
+            name: "SwiftTSL",
+            targets: ["SwiftTSL"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "swift-template-support-library",
+            name: "SwiftTSL",
             dependencies: []),
         .testTarget(
-            name: "swift-template-support-libraryTests",
-            dependencies: ["swift-template-support-library"]),
+            name: "SwiftTSL",
+            dependencies: ["SwiftTSL"]),
     ]
 )
